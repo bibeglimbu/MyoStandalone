39f78d15f14bce6091e789688e25cbcc434bb3a6
bibeglimbu <bibeglimbu@gmail.com>
2017-08-29 09:11:41 +0200

    initial commit

---
+++ MyoTest/MyoTest/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using MyoSharp.Communication;
+using MyoSharp.Device;
+using MyoSharp.Exceptions;
+using MyoSharp.Poses;
+using System.Diagnostics;
+using System.Windows.Threading;
+using System.Net.Sockets;
+using System.Net;
+
+namespace MyoTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Socket sending_socket;
+        IPAddress send_to_address;
+        MyoManager.MyoManagerClass myoManager = new MyoManager.MyoManagerClass();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            myoManager.InitMyoManagerHub(this);
+            
+        }
+
+        public void SendData(Int32 emg)
+        {
+
+            sending_socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            send_to_address = IPAddress.Parse("127.0.0.1");
+            IPEndPoint sending_end_point = new IPEndPoint(send_to_address, 11002);
+
+            SocketAsyncEventArgs socketEventArg = new SocketAsyncEventArgs();
+            socketEventArg.RemoteEndPoint = sending_end_point;
+
+            byte[] send_buffer = Encoding.UTF8.GetBytes(emg.ToString());
+
+            try
+            {
+                socketEventArg.SetBuffer(send_buffer, 0, send_buffer.Length);
+                sending_socket.SendToAsync(socketEventArg);
+                Debug.WriteLine("text sent");
+            }
+            catch
+            {
+                Debug.WriteLine("not initialized");
+            }
+        }
+
+        public void UpdateEmg(Int32 emg)
+        {
+            Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, new Action(
+                        () =>
+                        {
+                            EmgTxt.Text =emg.ToString();
+                        }));
+            SendData(emg);
+        }
+    }
+}
---
+++ MyoTest/MyoTest/MyoManager/MyoManagerClass.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+using MyoSharp.Communication;
+using MyoSharp.Device;
+using MyoSharp.Exceptions;
+using MyoSharp.Poses;
+
+namespace MyoTest.MyoManager
+{
+    class MyoManagerClass
+    {
+        IChannel channel;
+        public IHub hub;
+        MainWindow mWindow;
+
+        Int32[] firstPreEmgValue = new Int32[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
+        Int32[] secPreEmgValue = new Int32[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
+
+        public void InitMyoManagerHub(MainWindow m)
+        {
+            this.mWindow = m;
+            channel = Channel.Create(
+                ChannelDriver.Create(ChannelBridge.Create(),
+                MyoErrorHandlerDriver.Create(MyoErrorHandlerBridge.Create())));
+            hub = Hub.Create(channel);
+
+            // listen for when the Myo connects
+            hub.MyoConnected += (sender, e) =>
+            {
+                Debug.WriteLine("Myo {0} has connected!", e.Myo.Handle);
+                e.Myo.Vibrate(VibrationType.Short);
+                e.Myo.EmgDataAcquired += Myo_EmgDataAcquired;
+                e.Myo.OrientationDataAcquired += Myo_OrientationAcquired;
+                e.Myo.SetEmgStreaming(true);
+            };
+
+            // listen for when the Myo disconnects
+            hub.MyoDisconnected += (sender, e) =>
+            {
+                Debug.WriteLine("Oh no! It looks like {0} arm Myo has disconnected!", e.Myo.Arm);
+                e.Myo.SetEmgStreaming(false);
+                e.Myo.EmgDataAcquired -= Myo_EmgDataAcquired;
+            };
+
+            // start listening for Myo data
+            channel.StartListening();
+
+
+        }
+
+        private void Myo_EmgDataAcquired(object sender, EmgDataEventArgs e)
+        {
+
+            CheckEmg(e);
+        }
+
+        private void Myo_OrientationAcquired(object sender, OrientationDataEventArgs e)
+        {
+
+            CheckOrientation(e);
+        }
+
+        /// <summary>
+        /// Iterate through each emg sensor in myo and assign 1 if the sum of the first and second frame of emg has a sum of more than 20.
+        /// else assign 0. It means that much variation(100 to -100) was observed propotional to higher tension in muscle. 
+        /// 
+        /// </summary>
+        /// <param name="e"></param>
+        void CheckEmg(EmgDataEventArgs e)
+        {
+            int[] emgTension = new int[8];
+            Int32 avgTension = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                try
+                {
+                    if ((firstPreEmgValue[i] + secPreEmgValue[i] + Math.Abs(e.EmgData.GetDataForSensor(i))) <= 20)
+                    {
+                        emgTension[i] = 0;
+
+                    }
+                    else
+                    {
+                        emgTension[i] = 1;
+                    }
+
+                }
+                catch
+                {
+                    Debug.WriteLine("Myo not connceted");
+                }
+            }
+
+            //add all value from emgTension and assign it to avgTension
+            Array.ForEach(emgTension, delegate (int i) { avgTension += i; });
+            mWindow.UpdateEmg(avgTension);
+            LoadEmg(e);
+        }
+
+        /// <summary>
+        /// load and save the first and second frame of EMG[] value for comparision
+        /// </summary>
+        /// <param name="e"></param>
+        private void LoadEmg(EmgDataEventArgs e)
+        {
+            try
+            {
+                for (int i = 0; i < 7; i++)
+                {
+                    secPreEmgValue[i] = firstPreEmgValue[i];
+                    firstPreEmgValue[i] = Math.Abs(e.EmgData.GetDataForSensor(i));
+                }
+            }
+            catch
+            {
+                Debug.WriteLine("No emg value");
+            }
+
+        }
+
+
+        public void CheckOrientation(OrientationDataEventArgs e)
+        {
+            //mWindow.UpdateOrientation(e.Roll.ToString(), e.Yaw.ToString(), e.Pitch.ToString());
+        }
+    }
+}
